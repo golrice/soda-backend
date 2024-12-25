c8026f54674675cb33f72358700bc6d2e9c2985e
oxygen-anoxia <163303985+oxygen-anoxia@users.noreply.github.com>
2024-12-25 20:00:24 +0800

    modified:   application/urls.py (#1)
    modified:   application/views.py

--- application/urls.py
+++ application/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path("get-post/<str:post_name>", view=views.get_post, name="get_post"),
     path("get-posts", view=views.get_posts, name="get_posts"),
     path("create-post", view=views.create_post, name="create_post"),
+    path("login", view=views.verify_password, name="login"),
+    path("if_username_exist",view=views.if_username_exist,name="if_username_exist"),
+    path("Register",view=views.Register,name="Register")
 ]
