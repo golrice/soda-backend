81653bf4a5994baecff7cb92b9a581c2bbe459cb
oxygen-anoxia <163303985+oxygen-anoxia@users.noreply.github.com>
2024-12-26 20:02:52 +0800

    add userctrl,userprofile;modify urls;roll back views (#3)
    user register & login in

--- application/models.py
+++ application/models.py
@@ -3,8 +3,13 @@ from django.db import models
 class User(models.Model):
     username = models.CharField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
+    last_login = models.DateTimeField(null=True)
+    first_name = models.CharField(max_length=255,null=True)
+    last_name = models.CharField(max_length=255,null=True)
+    email = models.CharField(max_length=255,null=True)
+    date_joined = models.DateField(null=True)
     icon = models.URLField(null=True, blank=True)
-
+    intros = models.TextField(null=True)
     def __str__(self):
         return self.username
 
--- application/urls.py
+++ application/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 
 from . import views
-
+from . import userctrl
+from . import userprofile
 urlpatterns = [
     path("", views.index, name="index"),
     path("get-post/<str:post_name>", view=views.get_post, name="get_post"),
     path("get-posts", view=views.get_posts, name="get_posts"),
     path("create-post", view=views.create_post, name="create_post"),
-    path("login", view=views.verify_password, name="login"),
-    path("if_username_exist",view=views.if_username_exist,name="if_username_exist"),
-    path("Register",view=views.Register,name="Register")
+    path("login", view=userctrl.verify_password, name="login"),
+    path("if_username_exist",view=userctrl.if_username_exist,name="if_username_exist"),
+    path("Register",view=userctrl.Register,name="Register"),
+    path('get-profile', view=userprofile.get_profile, name='get_profile'),
 ]
